690f0000f1d74695d26568885c82e8ac968305c9
poziworld <poziworld@gmail.com>
2016-03-20 09:38:04 -0700

    v0.2.7: Added support for multiple-package Amazon return

--- modules/com_amazon/js/pwm.js
+++ modules/com_amazon/js/pwm.js
@@ -1,43 +1,62 @@
-// Loop through titles to check if there is a Hazmat Label
-var
-    $titles                   = document.getElementsByTagName( 'h3' )
-  , intL                      = $titles.length
-  , boolHazmatFound           = false
-  , strHazmatFoundClass       = 'pwmExtHazmatLabelFound'
-  , strHazmatNotFoundClass    = 'pwmExtHazmatLabelNotFound'
-  , strHazmatClass            = strHazmatNotFoundClass
-  , strSnailMailSectionId     = 'postSendSnailMailSection'
-  , strSnailMailFoundClass    = 'pwmExtSnailMailSectionFound'
-  , strSnailMailNotFoundClass = 'pwmExtSnailMailSectionNotFound'
-  , strSnailMailClass         = strSnailMailFoundClass
-  ;
+( function() {
+  'use strict';
 
-for ( var intI = intL - 1; intI >= 0; intI-- ) {
-  if ( $titles[ intI ].innerText === 'Hazmat Label' ) {
-    boolHazmatFound = true;
-    break;
+  // Loop through titles to check if there is a Hazmat Label
+  var $titles = document.getElementsByTagName( 'h3' )
+    , intL = $titles.length
+
+      // Hazmat
+    , boolHazmatFound = false
+    , strHazmatFoundClass = 'pwmExtHazmatLabelFound'
+    , strHazmatNotFoundClass = 'pwmExtHazmatLabelNotFound'
+    , strHazmatClass = strHazmatNotFoundClass
+
+      // Snail mail
+    , strSnailMailSectionId = 'postSendSnailMailSection'
+    , strSnailMailFoundClass = 'pwmExtSnailMailSectionFound'
+    , strSnailMailNotFoundClass = 'pwmExtSnailMailSectionNotFound'
+    , strSnailMailClass = strSnailMailFoundClass
+
+      // Multiple packages
+    , strMultiplePackagesClass = 'pwmExtMultiplePackagesNotFound'
+    , strMultiplePackagesFoundClass = 'pwmExtMultiplePackagesFound'
+    , strMultiplePackagesIndicator = 'Package (1/'
+    ;
+
+  for ( var intI = intL - 1; intI >= 0; intI-- ) {
+    if ( $titles[ intI ].innerText === 'Hazmat Label' ) {
+      boolHazmatFound = true;
+      break;
+    }
   }
-}
 
-if ( boolHazmatFound ) {
-  strHazmatClass = strHazmatFoundClass
-}
+  if ( boolHazmatFound ) {
+    strHazmatClass = strHazmatFoundClass
+  }
 
-if ( ! document.contains( document.getElementById( strSnailMailSectionId ) ) ) {
-  strSnailMailClass = strSnailMailNotFoundClass;
-}
+  // Check snail mail section presence
+  if ( ! document.contains( document.getElementById( strSnailMailSectionId ) ) ) {
+    strSnailMailClass = strSnailMailNotFoundClass;
+  }
 
-var $pageFirstChild = document.body;
+  // Check multiple packages presence
+  if ( ~ document.documentElement.textContent.indexOf( strMultiplePackagesIndicator ) ) {
+    strMultiplePackagesClass = strMultiplePackagesFoundClass;
+  }
 
-$pageFirstChild.classList.add(
-    strActiveClass
-  , strHazmatClass
-  , strSnailMailClass
-);
+  var $pageFirstChild = document.body;
 
-// Append the Toggle to the page
-$pageFirstChild.appendChild( $toggle );
+  $pageFirstChild.classList.add(
+      strActiveClass
+    , strHazmatClass
+    , strSnailMailClass
+    , strMultiplePackagesClass
+  );
 
-$toggle.onclick = function() {
-  $pageFirstChild.classList.toggle( strActiveClass );
-};
+  // Append the Toggle to the page
+  $pageFirstChild.appendChild( $toggle );
+
+  $toggle.onclick = function() {
+    $pageFirstChild.classList.toggle( strActiveClass );
+  };
+} )();
