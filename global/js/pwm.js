0eb0d46961ac5afbe441ed6cdf9c98b39a988d74
poziworld <poziworld@gmail.com>
2018-07-05 20:57:13 -0700

    New: Firefox & Edge support

--- global/js/pwm.js
+++ global/js/pwm.js
@@ -1,5 +1,18 @@
+var extensionApisNamespace;
 var strActiveClass = 'pwmExtensionActive';
 var $toggle = document.createElement( 'button' );
 
+/**
+ * https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Chrome_incompatibilities
+ */
+
+if ( typeof browser !== 'undefined' ) {
+  extensionApisNamespace = browser;
+}
+else {
+  extensionApisNamespace = chrome;
+}
+
 $toggle.id = 'pwmExtensionToggleCta';
-$toggle.title = chrome.i18n.getMessage( 'toggleTitle' );
+$toggle.title = extensionApisNamespace.i18n.getMessage( 'toggleTitle' );
+$toggle.setAttribute( 'style', 'background-image: url(' + extensionApisNamespace.runtime.getURL( 'global/img/pwm-icon-16.png' ) + ') !important' );
